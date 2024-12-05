dc6794e14fd387b995c2ccaa566ac7fceba94070
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-06 00:21:46 +0900

    refactor: change the navigation back to stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,38 +1,8 @@
 import { MaterialIcons } from '@expo/vector-icons'
-import { Drawer } from 'expo-router/drawer'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { Stack } from 'expo-router'
 
 export default function Layout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer
-        screenOptions={{
-          drawerActiveTintColor: "#000000",
-          drawerInactiveTintColor: "#999999",
-          headerShown: false
-        }}
-      >
-        <Drawer.Screen
-          name="index"
-          options={{
-            drawerLabel: "Entrar",
-            title: "Entrar",
-            drawerIcon: ({ color, size }) => (
-              <MaterialIcons name='home' size={size} color={color} />
-            )
-          }}
-        />
-        <Drawer.Screen
-          name="sign-up"
-          options={{
-            drawerLabel: "Criar Conta",
-            title: "Criar Conta",
-            drawerIcon: ({ color, size }) => (
-              <MaterialIcons name='add' size={size} color={color} />
-            )
-          }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
+    <Stack screenOptions={{ headerShown: false }} />
   )
 }
